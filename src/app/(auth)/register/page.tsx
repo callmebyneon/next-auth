65ec05edba08e6c010f452ad8476435ba9301a5a
YI Na-yeon <2n.ayeon.yi@gmail.com>
2024-06-14 01:31:28 +0900

    Add: 회원가입 폼 작성 주의사항 추가

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -5,6 +5,14 @@ export default function Register() {
     <>
       <div className="flex flex-col gap-2">
         <h1 className="text-center">Register</h1>
+        <div className="w-80 px-3 py-2 mt-2 text-sm bg-orange-100 text-orange-500 rounded-md border border-orange-500">
+          <p>
+            <b>Notice:</b> <br />
+            If you register successful, move login page and the submitted data
+            save to personal MongoBD. Be careful <em>NOT</em> to enter personal
+            data.
+          </p>
+        </div>
         <RegisterForm />
       </div>
     </>

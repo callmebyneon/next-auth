689befa187621a80df5f53229e4f72a241f5cd7f
YI Na-yeon <2n.ayeon.yi@gmail.com>
2024-06-13 17:44:39 +0900

    Add: move link to register page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -6,11 +6,6 @@ export default function Login() {
   return (
     <>
       <div className="flex flex-col gap-2">
-        <div className="flex justify-start">
-          <Link href="/register">
-            <BiArrowBack />
-          </Link>
-        </div>
         <h1 className="text-center">Login</h1>
         <LoginForm />
       </div>
--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,11 +1,12 @@
 import { githubLogin, login } from "@/libs/action";
+import Link from "next/link";
 
 export default function LoginForm() {
   return (
     <>
       <form
         action={login}
-        className="flex flex-col gap-2 justify-start w-80 my-4"
+        className="flex flex-col gap-2 justify-start w-80 mt-4"
       >
         <input type="email" name="email" placeholder="YourEmail@example.com" />
         <input
@@ -21,6 +22,12 @@ export default function LoginForm() {
       >
         <button className="bg-black text-white">Github Log In</button>
       </form>
+      <Link
+        href="/register"
+        className="no-underline mt-4 p-2 rounded-md bg-blue-500 text-white"
+      >
+        <div className=" block text-center">Go To Register</div>
+      </Link>
     </>
   );
 }

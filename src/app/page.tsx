c3d1593438971f607a349c9d3bb43b8840774b11
YI Na-yeon <2n.ayeon.yi@gmail.com>
2024-06-14 01:29:37 +0900

    Update: index 페이지 h1 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default async function Home() {
   const session = await getSession();
   return (
     <>
-      <h1>Home Page</h1>
+      <h1>Next Auth Demo</h1>
       {session && <pre>session: {JSON.stringify(session, null, 2)}</pre>}
     </>
   );
